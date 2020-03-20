f616c2a0f02745aab850944fde3dcf2412aea166
preranaandure <prerana.andure@dbca.wa.gov.au>
2020-03-20 17:18:58 +0800

    Equipment tab design for Filming application.

--- commercialoperator/frontend/commercialoperator/src/components/common/filming/equipment.vue
+++ commercialoperator/frontend/commercialoperator/src/components/common/filming/equipment.vue
@@ -13,19 +13,35 @@
                 <div class="" >                        
                     <div class="form-horizontal col-sm-12 borderDecoration">
                         
-                        <div class="form-group">
+                        <div class="form-group col-sm-12" >
                             <div class="row">
                                 <div class="col-sm-6">
                                     <label class="control-label pull-left"  for="Name">Number and type of cameras to be used</label>
                                 </div>
                                 <div class="col-sm-6">
-                                    <!-- <input type="text" class="form-control" v-model="proposal.equiment_filming.num_cameras" name="num_cameras" :disabled="proposal.readonly || proposal.pending_amendment_request || proposal.is_amendment_proposal"> -->
-                                    <input type="text" class="form-control" name="num_cameras" :disabled="proposal.readonly || proposal.pending_amendment_request || proposal.is_amendment_proposal">
+                                    <ul class="list-inline"  >
+                                        <li class="form-check list-inline-item">
+                                            <input  class="form-check-input" ref="Radio" type="radio"  v-model="proposal.filming_equipment.vehicle_owned" :value="true" data-parsley-required :disabled="proposal.readonly" name="vehicle_owned"/>
+                                            Owned
+                                        </li>
+                                        <li class="form-check list-inline-item">
+                                            <input  class="form-check-input" ref="Radio" type="radio"  v-model="proposal.filming_equipment.vehicle_owned" :value="false" data-parsley-required :disabled="proposal.readonly" name="vehicle_owned"/>
+                                            Hired
+                                        </li>
+                                    </ul>  
                                 </div>
                             </div>
                             <div class="row">&nbsp;</div>
+                            <div class="">
+                                <!-- <label class="control-label">Provide details of every vehicle you plan to use when accessing the parks</label> -->
+                                <VehicleTable :url="vehicles_url" :proposal="proposal" :access_types="access_types" ref="vehicles_table"></VehicleTable>
+                            </div>
                         </div> 
                     </div>
+                    <div class="form-horizontal col-sm-12 borderDecoration">
+                        <VesselTable :url="vessels_url" :proposal="proposal" ref="vessel_table"></VesselTable>
+                    </div>
+
                 </div>
             </div>                
 
@@ -36,6 +52,9 @@
 </template>
 
 <script>
+import {helpers,api_endpoints} from "@/utils/hooks.js"
+import VehicleTable from '@/components/common/vehicle_table.vue'
+import VesselTable from '@/components/common/vessel_table.vue' 
     export default {
         props:{
             proposal:{
@@ -47,14 +66,40 @@
             let vm = this;
             return{
                 lBody: 'lBody'+vm._uid,
-                values:null
+                values:null,
+                access_types:[],
+                vehicles_url: helpers.add_endpoint_json(api_endpoints.proposals,vm.$route.params.proposal_id+'/vehicles'),
+                vessels_url: helpers.add_endpoint_json(api_endpoints.proposals,vm.$route.params.proposal_id+'/vessels'),
             }
         },
+        components:{
+            VehicleTable,
+            VesselTable,
+        },
         methods:{
+            fetchAccessTypes: function(){
+            let vm = this;
+            vm.$http.get(api_endpoints.access_types).then((response) => {
+                vm.access_types = response.body;
+                console.log(vm.access_types);
+            },(error) => {
+                console.log(error);
+            } );
+        },
+        },
+        mounted: function(){
+            let vm=this;
+            vm.fetchAccessTypes();
         }
     }
 </script>
 
 <style lang="css" scoped>
+.borderDecoration {
+    border: 1px solid;
+    border-radius: 5px;
+    padding: 5px;
+    margin-top: 5px;
+}
 </style>
 
