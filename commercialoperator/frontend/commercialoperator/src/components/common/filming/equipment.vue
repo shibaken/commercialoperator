fc63ea2b4ff524f894e581db7364f6eb507d185e
preranaandure <prerana.andure@dbca.wa.gov.au>
2020-03-23 11:58:48 +0800

    Equipment tab design for Filming application.

--- commercialoperator/frontend/commercialoperator/src/components/common/filming/equipment.vue
+++ commercialoperator/frontend/commercialoperator/src/components/common/filming/equipment.vue
@@ -16,7 +16,7 @@
                         <div class="form-group col-sm-12" >
                             <div class="row">
                                 <div class="col-sm-6">
-                                    <label class="control-label pull-left"  for="Name">Number and type of cameras to be used</label>
+                                    <label class="control-label pull-left"  for="Name">Are your vehicles </label>
                                 </div>
                                 <div class="col-sm-6">
                                     <ul class="list-inline"  >
@@ -42,6 +42,99 @@
                         <VesselTable :url="vessels_url" :proposal="proposal" ref="vessel_table"></VesselTable>
                     </div>
 
+                    <div class="form-horizontal col-sm-12 borderDecoration">
+                        <div class="row">&nbsp;</div>
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <label class="control-label pull-right"  for="Name">Are you using a Remotely Pilotaed Aircraft (RPS) for you filming and/ or photography activities?</label>
+                            </div>
+                            <div class="col-sm-6" style="margin-bottom: 5px">
+                                <ul class="list-inline"  >
+                                    <li class="form-check list-inline-item">
+                                            <input  class="form-check-input" ref="Radio" type="radio"  v-model="proposal.filming_equipment.rps_used" :value="true" data-parsley-required :disabled="proposal.readonly" name="rps_used"/>
+                                            Yes
+                                    </li>
+                                    <li class="form-check list-inline-item">
+                                            <input  class="form-check-input" ref="Radio" type="radio"  v-model="proposal.filming_equipment.rps_used" :value="false" data-parsley-required :disabled="proposal.readonly" name="rps_used"/>
+                                            No
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="row">&nbsp;</div>
+                        <div class="row" v-if="proposal.filming_equipment.rps_used">
+                            <div class="col-sm-6">
+                                <label class="control-label pull-right"  for="Name">Is its weight over two kilogram?</label>
+                            </div>
+                            <div class="col-sm-6" style="margin-bottom: 5px">
+                                <ul class="list-inline"  >
+                                    <li class="form-check list-inline-item">
+                                            <input  class="form-check-input" ref="Radio" type="radio"  v-model="proposal.filming_equipment.rps_overweight" :value="true" data-parsley-required :disabled="proposal.readonly" name="rps_overweight"/>
+                                            Yes
+                                    </li>
+                                    <li class="form-check list-inline-item">
+                                            <input  class="form-check-input" ref="Radio" type="radio"  v-model="proposal.filming_equipment.rps_overweight" :value="false" data-parsley-required :disabled="proposal.readonly" name="rps_overweight"/>
+                                            No
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="row">&nbsp;</div>
+                        <div class="row" v-if="proposal.filming_equipment.rps_used">
+                            <div class="col-sm-6">
+                                <label class="control-label pull-right"  for="Name">Attache CASA Remotely piloted aircraft operator's certificate (ReOC) or licence (RePL)</label>
+                            </div>
+                            <div class="col-sm-6" style="margin-bottom: 5px">
+                                <div class="col-sm-6" style="margin-bottom: 5px">
+                                      <FileField :proposal_id="proposal.id" isRepeatable="false" name="rps_certificate" :id="'proposal'+proposal.id" :readonly="proposal.readonly" ref="rps_cert"></FileField>  
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="form-horizontal col-sm-12 borderDecoration">
+                        <div class="row">&nbsp;</div>
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <label class="control-label pull-right"  for="Name">Number and type of cameras to be used </label>
+                            </div>
+                            <div class="col-sm-6" style="margin-bottom: 5px">
+                                <div class="col-sm-6" style="margin-bottom: 5px">
+                                      <input type="text" class="form-control" name="num_cameras" placeholder="" :disabled="proposal.readonly" v-model="proposal.filming_equipment.num_cameras">   
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">&nbsp;</div>
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <label class="control-label pull-right"  for="Name">Will any stuctures or facilities be erected or does the area require any alteration to occur to allow the filming?</label>
+                            </div>
+                            <div class="col-sm-6" style="margin-bottom: 5px">
+                                <ul class="list-inline"  >
+                                    <li class="form-check list-inline-item">
+                                            <input  class="form-check-input" ref="Radio" type="radio"  v-model="proposal.filming_equipment.alteration_required" :value="true" data-parsley-required :disabled="proposal.readonly" name="alteration_required"/>
+                                            Yes
+                                    </li>
+                                    <li class="form-check list-inline-item">
+                                            <input  class="form-check-input" ref="Radio" type="radio"  v-model="proposal.filming_equipment.alteration_required" :value="false" data-parsley-required :disabled="proposal.readonly" name="alteration_required"/>
+                                            No
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                        <div class="row">&nbsp;</div>
+                        <div class="row">
+                            <div class="col-sm-6">
+                                <label class="control-label pull-right"  for="Name">List any other equipment that may be used during this operations, for example, motors, generators/lights, dolly tracks, shade shelters, portable toilets, firearms/ weapons, animal traps/ snares or bait, cooking equipment <small>(Firearms, snare, traps and baits are not to be carrid on CALM land unless a licence is obtained. Licences are issued only for scientific purposes)</small>></label>
+                            </div>
+                            <div class="col-sm-6" style="margin-bottom: 5px">
+                                <div class="col-sm-6" style="margin-bottom: 5px">
+                                      <textarea type="text" class="form-control" name="num_cameras" placeholder="" :disabled="proposal.readonly" v-model="proposal.filming_equipment.other_equipments"></textarea>   
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
                 </div>
             </div>                
 
@@ -54,7 +147,8 @@
 <script>
 import {helpers,api_endpoints} from "@/utils/hooks.js"
 import VehicleTable from '@/components/common/vehicle_table.vue'
-import VesselTable from '@/components/common/vessel_table.vue' 
+import VesselTable from '@/components/common/vessel_table.vue'
+import FileField from '@/components/forms/filefield.vue' 
     export default {
         props:{
             proposal:{
@@ -75,6 +169,7 @@ import VesselTable from '@/components/common/vessel_table.vue'
         components:{
             VehicleTable,
             VesselTable,
+            FileField,
         },
         methods:{
             fetchAccessTypes: function(){
