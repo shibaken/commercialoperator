1024ef7a8c1c7cf6c14477dc2cf353e5d3ec53cf
preranaandure <prerana.andure@dbca.wa.gov.au>
2020-05-22 11:15:04 +0800

    Admin form for District assessor and approver group.

--- commercialoperator/components/proposals/admin.py
+++ commercialoperator/components/proposals/admin.py
@@ -18,7 +18,7 @@ from commercialoperator.components.main.models import (
     Zone,
     RequiredDocument,
     Question,
-    GlobalSettings
+    GlobalSettings,
 )
 #from commercialoperator.components.main.models import Activity, SubActivityLevel1, SubActivityLevel2, SubCategory
 from reversion.admin import VersionAdmin
@@ -242,3 +242,52 @@ class QuestionAdmin(admin.ModelAdmin):
 class SectionAdmin(admin.ModelAdmin):
     list_display = [f.name for f in ApplicationFeeInvoice._meta.fields]
 
+
+@admin.register(models.DistrictProposalAssessorGroup)
+class DistrictProposalAssessorGroupAdmin(admin.ModelAdmin):
+    list_display = ['name','default']
+    filter_horizontal = ('members',)
+    form = forms.DistrictProposalAssessorGroupAdminForm
+    #readonly_fields = ['default']
+    #readonly_fields = ['default', 'regions', 'activities']
+
+    def get_actions(self, request):
+        actions =  super(DistrictProposalAssessorGroupAdmin, self).get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
+
+    def has_delete_permission(self, request, obj=None):
+        if self.model.objects.count() == 1:
+            return False
+        return super(DistrictProposalAssessorGroupAdmin, self).has_delete_permission(request, obj)
+
+    def has_add_permission(self, request):
+        if self.model.objects.count() > 0:
+            return False
+        return super(DistrictProposalAssessorGroupAdmin, self).has_add_permission(request)
+
+
+@admin.register(models.DistrictProposalApproverGroup)
+class DistrictProposalApproverGroupAdmin(admin.ModelAdmin):
+    list_display = ['name','default']
+    filter_horizontal = ('members',)
+    form = forms.DistrictProposalApproverGroupAdminForm
+    #readonly_fields = ['default']
+    #readonly_fields = ['default', 'regions', 'activities']
+
+    def get_actions(self, request):
+        actions =  super(DistrictProposalApproverGroupAdmin, self).get_actions(request)
+        if 'delete_selected' in actions:
+            del actions['delete_selected']
+        return actions
+
+    def has_delete_permission(self, request, obj=None):
+        if self.model.objects.count() == 1:
+            return False
+        return super(DistrictProposalApproverGroupAdmin, self).has_delete_permission(request, obj)
+
+    def has_add_permission(self, request):
+        if self.model.objects.count() > 0:
+            return False
+        return super(DistrictProposalApproverGroupAdmin, self).has_add_permission(request)
