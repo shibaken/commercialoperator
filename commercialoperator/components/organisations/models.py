e833de3b3ee8af03049b575cea0c6ce5c0129ce8
preranaandure <prerana.andure@dbca.wa.gov.au>
2020-09-09 11:08:55 +0800

    Allow non-consultant user to request Organisation Request for existing Organisation without admin user

--- commercialoperator/components/organisations/models.py
+++ commercialoperator/components/organisations/models.py
@@ -11,7 +11,7 @@ from django.core.validators import MaxValueValidator, MinValueValidator
 from ledger.accounts.models import Organisation as ledger_organisation
 from ledger.accounts.models import EmailUser,RevisionedMixin #,Document
 from commercialoperator.components.main.models import UserAction,CommunicationsLogEntry, Document
-from commercialoperator.components.organisations.utils import random_generator, can_admin_org
+from commercialoperator.components.organisations.utils import random_generator, can_admin_org, has_atleast_one_admin
 from commercialoperator.components.organisations.emails import (
                         send_organisation_request_accept_email_notification,
                         send_organisation_request_decline_email_notification,
@@ -185,7 +185,10 @@ class Organisation(models.Model):
                 except Organisation.DoesNotExist:
                     exists = False
             if exists:
-                return {'exists': exists, 'id': org.id,'first_five':org.first_five}
+                if has_atleast_one_admin(org):
+                    return {'exists': exists, 'id': org.id,'first_five':org.first_five}
+                else:
+                    return {'exists': has_atleast_one_admin(org) }
             return {'exists': exists }
 
         except:
@@ -600,7 +603,7 @@ class OrganisationRequest(models.Model):
         ('employee','Employee'),
         ('consultant','Consultant')
     )
-    name = models.CharField(max_length=128, unique=True)
+    name = models.CharField(max_length=128)
     abn = models.CharField(max_length=50, null=True, blank=True, verbose_name='ABN')
     requester = models.ForeignKey(EmailUser)
     assigned_officer = models.ForeignKey(EmailUser, blank=True, null=True, related_name='org_request_assignee')
