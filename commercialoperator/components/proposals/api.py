1717e56b0837ad3eab3cc93d8b5d0d5e629f9816
preranaandure <preranapj07@gmail.com>
2026-09-09 16:26:03 +0800

    Copy changes from patch_7c8ff96 branch

--- commercialoperator/components/proposals/api.py
+++ commercialoperator/components/proposals/api.py
@@ -1645,7 +1645,7 @@ class ProposalViewSet(viewsets.ModelViewSet):
         try:
             with transaction.atomic():
                 instance = self.get_object()
-                is_onhold =  eval(request.data.get('onhold'))
+                is_onhold =  request.data.get('onhold').lower()=='true'
                 data = {}
                 if is_onhold:
                     data['type'] = u'onhold'
@@ -1691,7 +1691,7 @@ class ProposalViewSet(viewsets.ModelViewSet):
         try:
             with transaction.atomic():
                 instance = self.get_object()
-                is_with_qaofficer =  eval(request.data.get('with_qaofficer'))
+                is_with_qaofficer =  request.data.get('with_qaofficer').lower()=='true'
                 data = {}
                 if is_with_qaofficer:
                     data['type'] = u'with_qaofficer'
